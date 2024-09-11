7e356f33142c6197edfb5baa270486565889b091
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2024-09-11 19:14:41 -0300

    fixed: service

--- src/main/java/unicap/si/microservicos/api/buscaCep/service/PointDataService.java
+++ src/main/java/unicap/si/microservicos/api/buscaCep/service/PointDataService.java
@@ -1,6 +1,5 @@
 package unicap.si.microservicos.api.buscaCep.service;
 
-import org.apache.http.client.methods.HttpGet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import unicap.si.microservicos.api.buscaCep.exception.CepNotFoundException;
