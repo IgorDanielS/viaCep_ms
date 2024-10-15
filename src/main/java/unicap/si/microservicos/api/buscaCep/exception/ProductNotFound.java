8a35a21f1b87ba20d1d8b52f822b73667192f4a7
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2024-10-15 19:56:23 -0300

    azure conectado igor

--- src/main/java/unicap/si/microservicos/api/buscaCep/exception/ProductNotFound.java
+++
@@ -1,7 +0,0 @@
-package unicap.si.microservicos.api.buscaCep.exception;
-
-public class ProductNotFound extends Exception{
-    public ProductNotFound(String message){
-        super(message);
-    }
-}
